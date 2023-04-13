24423049c9ae0f2a3f9d986ab4512251f98a405e
Martin Tazon <martin.tazon.tdr@gmail.com>
2023-04-13 14:54:54 +0200

    Added experiments.txt

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-BASEDIR=../lab1/DDI/
+BASEDIR=../../lab_resources/DDI/
+#BASEDIR=../lab1/DDI/
 
-# ./corenlp-server.sh -quiet true -port 9000 -timeout 15000  &
-# sleep 10
+./corenlp-server.sh -quiet true -port 9000 -timeout 15000  &
+sleep 1
+
+exit
 
 # extract features
 echo "Extracting features"
-# python3 extract-features.py $BASEDIR/data/devel/
-python3 extract-features.py $BASEDIR/data/devel/ > devel.cod &
-python3 extract-features.py $BASEDIR/data/train/ | tee train.cod | cut -f4- > train.cod.cl
+python3 -i extract-features.py $BASEDIR/data/devel/ > devel.cod &
+#python3 extract-features.py $BASEDIR/data/train/ | tee train.cod | cut -f4- > train.cod.cl
 
 kill `cat /tmp/corenlp-server.running`
 
@@ -23,3 +25,8 @@ python3 predict-sklearn.py model.joblib vectorizer.joblib < devel.cod > devel.ou
 echo "Evaluating results..."
 python3 evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel.stats
 
+SAVEFILE=experiments.txt
+i=`grep "#### EXPERIMENT" $SAVEFILE | wc -l`
+echo "#### EXPERIMENT $i ####" >> $SAVEFILE
+cat devel.stats >> $SAVEFILE
+echo "#######################" >> $SAVEFILE
